da490242b15695fcbbf967996ccd7f5b19ac67b7
Shawn O'Banion <shawn.obanion@gmail.com>
2011-10-31 16:43:49 -0500

    Fixed path to .xcodeproj

--- Jukebox/ci/build.sh
+++ Jukebox/ci/build.sh
@@ -1,1 +1,1 @@
-/Developer/usr/bin/xcodebuild -project Jukebox.xcodeproj -target Jukebox -sdk iphonesimulator4.3 -configuration Debug clean build
+/Developer/usr/bin/xcodebuild -project Jukebox/Jukebox.xcodeproj -target Jukebox -sdk iphonesimulator4.3 -configuration Debug clean build
