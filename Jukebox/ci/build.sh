a2438ac0ec2a89dbb01a51171f0d25cf96baa028
Shawn O'Banion <shawn.obanion@gmail.com>
2011-10-31 16:41:18 -0500

    Changed to iphonesimulator4.3

--- Jukebox/ci/build.sh
+++ Jukebox/ci/build.sh
@@ -1,1 +1,1 @@
-/Developer/usr/bin/xcodebuild -project Jukebox.xcodeproj -target Jukebox -sdk iphonesimulator5.0 -configuration Debug clean build
+/Developer/usr/bin/xcodebuild -project Jukebox.xcodeproj -target Jukebox -sdk iphonesimulator4.3 -configuration Debug clean build
