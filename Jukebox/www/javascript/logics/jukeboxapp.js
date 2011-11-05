fce9db1158a44d86dfa6b9ecdefba5dab4e621a9
Thanapon Noraset <flcl.tvboy@gmail.com>
2011-11-05 07:17:08 -0500

    interface to select songs

---
+++ Jukebox/www/javascript/logics/jukeboxapp.js
@@ -0,0 +1,33 @@
+var Jukebox = Jukebox || {};
+
+
+/*DOM object
+ *contains all functions for rendering html interface from data
+ */
+Jukebox.DOM = function() {};
+Jukebox.DOM.prototype.renderListMediaItems = function(items) {
+    if(items.length <= 0) {
+        return '';
+    }
+    var html = '';
+	for (i in items) {
+		html += this.renderListMediaItem(items[i]);
+	}
+    html += '</ul>';
+	return html;
+};
+
+Jukebox.DOM.prototype.renderListMediaItem = function(item) {
+    var html = '<li class="ui-li ui-li-static ui-body-c" id="'+ item.persistentID +'">'
+    html += '<p class="ui-li-aside ui-li-desc"><strong>'+this.formatTimeInterval(item.playbackDuration)+'</strong></p>';
+    html += '<h3>'+item.title+'</h3>';
+    html += '<p>' + item.albumTitle + ' - ' + item.artist + '</p>';
+    html += '</li>';
+    return html;
+};
+
+Jukebox.DOM.prototype.formatTimeInterval = function(seconds) {
+    return (new Date).clearTime()
+                     .addSeconds(seconds)
+                     .toString('mm:ss');
+};
